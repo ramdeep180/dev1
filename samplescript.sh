e73f7d5b014a9bc2a282be1e4513965519409826
ramdeep <ramdeep180@gmail.com>
2020-03-28 13:44:27 +0530

    commits added

--- samplescript.sh
+++ samplescript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+echo "________running meta data now"
 PH=$(curl -sL http://169.254.169.254/latest/meta-data/public-hostname)
 PSG=$(curl -sL http://169.254.169.254/latest/meta-data/security-groups)
 PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
 TYPE=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
 ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
-
+echo "completed....!!!!"
 echo "The public host name is $PH"
 echo "The public ip is $PIP"
 echo "the pub sec group is $PSG"
