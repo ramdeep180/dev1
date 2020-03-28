40078581542cd4d8917177326126c6a7b79a0917
root <root@ip-10-0-1-113.ec2.internal>
2020-03-28 05:19:29 +0000

    Updated p name and p ip information

--- samplescript.sh
+++ samplescript.sh
@@ -5,8 +5,8 @@ PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
 TYPE=$(curl -sL http://169.254.169.254/latest/meta-data/instance-type)
 ID=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
 
-echo "$PH"
-echo "$PIP"
+echo "The public host name is $PH"
+echo "The public ip is $PIP"
 echo "$PSG"
 echo "$TYPE"
 echo "$ID" 
